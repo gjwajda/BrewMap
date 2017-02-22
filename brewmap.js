63d6c636976e4592243d78a34d989d6c7b68978c
Greg <greg.wajda@comcast.net>
2017-02-22 16:47:18 -0600

    sidebar created

--- brewmap.js
+++ brewmap.js
@@ -1,192 +1,257 @@
- function initMap() {
-     // Styles a map in night mode.
-     var map = new google.maps.Map(document.getElementById('map'), {
-         center: { lat: 40.674, lng: -73.945 },
-         zoom: 5,
-         styles: [{
-                 "elementType": "geometry",
-                 "stylers": [{
-                     "color": "#ebe3cd"
-                 }]
-             },
-             {
-                 "elementType": "labels.text.fill",
-                 "stylers": [{
-                     "color": "#523735"
-                 }]
-             },
-             {
-                 "elementType": "labels.text.stroke",
-                 "stylers": [{
-                     "color": "#f5f1e6"
-                 }]
-             },
-             {
-                 "featureType": "administrative",
-                 "elementType": "geometry.stroke",
-                 "stylers": [{
-                     "color": "#c9b2a6"
-                 }]
-             },
-             {
-                 "featureType": "administrative.land_parcel",
-                 "elementType": "geometry.stroke",
-                 "stylers": [{
-                     "color": "#dcd2be"
-                 }]
-             },
-             {
-                 "featureType": "administrative.land_parcel",
-                 "elementType": "labels.text.fill",
-                 "stylers": [{
-                     "color": "#ae9e90"
-                 }]
-             },
-             {
-                 "featureType": "landscape.natural",
-                 "elementType": "geometry",
-                 "stylers": [{
-                     "color": "#dfd2ae"
-                 }]
-             },
-             {
-                 "featureType": "poi",
-                 "elementType": "geometry",
-                 "stylers": [{
-                     "color": "#dfd2ae"
-                 }]
-             },
-             {
-                 "featureType": "poi",
-                 "elementType": "labels.text.fill",
-                 "stylers": [{
-                     "color": "#93817c"
-                 }]
-             },
-             {
-                 "featureType": "poi.business",
-                 "stylers": [{
-                     "visibility": "off"
-                 }]
-             },
-             {
-                 "featureType": "poi.park",
-                 "elementType": "geometry.fill",
-                 "stylers": [{
-                     "color": "#a5b076"
-                 }]
-             },
-             {
-                 "featureType": "poi.park",
-                 "elementType": "labels.text",
-                 "stylers": [{
-                     "visibility": "off"
-                 }]
-             },
-             {
-                 "featureType": "poi.park",
-                 "elementType": "labels.text.fill",
-                 "stylers": [{
-                     "color": "#447530"
-                 }]
-             },
-             {
-                 "featureType": "road",
-                 "elementType": "geometry",
-                 "stylers": [{
-                     "color": "#f5f1e6"
-                 }]
-             },
-             {
-                 "featureType": "road.arterial",
-                 "elementType": "geometry",
-                 "stylers": [{
-                     "color": "#fdfcf8"
-                 }]
-             },
-             {
-                 "featureType": "road.highway",
-                 "elementType": "geometry",
-                 "stylers": [{
-                     "color": "#f8c967"
-                 }]
-             },
-             {
-                 "featureType": "road.highway",
-                 "elementType": "geometry.stroke",
-                 "stylers": [{
-                     "color": "#e9bc62"
-                 }]
-             },
-             {
-                 "featureType": "road.highway.controlled_access",
-                 "elementType": "geometry",
-                 "stylers": [{
-                     "color": "#e98d58"
-                 }]
-             },
-             {
-                 "featureType": "road.highway.controlled_access",
-                 "elementType": "geometry.stroke",
-                 "stylers": [{
-                     "color": "#db8555"
-                 }]
-             },
-             {
-                 "featureType": "road.local",
-                 "elementType": "labels.text.fill",
-                 "stylers": [{
-                     "color": "#806b63"
-                 }]
-             },
-             {
-                 "featureType": "transit.line",
-                 "elementType": "geometry",
-                 "stylers": [{
-                     "color": "#dfd2ae"
-                 }]
-             },
-             {
-                 "featureType": "transit.line",
-                 "elementType": "labels.text.fill",
-                 "stylers": [{
-                     "color": "#8f7d77"
-                 }]
-             },
-             {
-                 "featureType": "transit.line",
-                 "elementType": "labels.text.stroke",
-                 "stylers": [{
-                     "color": "#ebe3cd"
-                 }]
-             },
-             {
-                 "featureType": "transit.station",
-                 "elementType": "geometry",
-                 "stylers": [{
-                     "color": "#dfd2ae"
-                 }]
-             },
-             {
-                 "featureType": "water",
-                 "elementType": "geometry.fill",
-                 "stylers": [{
-                     "color": "#b9d3c2"
-                 }]
-             },
-             {
-                 "featureType": "water",
-                 "elementType": "labels.text.fill",
-                 "stylers": [{
-                     "color": "#92998d"
-                 }]
-             }
-         ]
-     });
+var brewery_markers = new Array; //List of brewery info
 
-     var marker = new google.maps.Marker({
-         position: { lat: 40.674, lng: -73.945 },
-         map: map,
-         title: 'Hello World!'
-     });
- }
+//Mark the breweries on the map
+function markbrewery(arr) {
+    var i;
+    for (i = 0; i < arr.length; i++) {
+        var marker = new google.maps.Marker({
+            position: { lat: arr[i].latitude, lng: arr[i].longitude },
+            map: map,
+            title: 'Hello World!'
+        });
+        brewery_markers.push(marker);
+    }
+    document.getElementById("id01").innerHTML = out;
+
+
+}
+
+//Get Brewery info from the API
+function get_breweries() {
+    var xmlhttp = new XMLHttpRequest();
+    var url = "http://api.brewerydb.com/v2/?key=481d514448fd7365873ba9501d928e10/locations";
+
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myArr = JSON.parse(this.responseText);
+            markbrewery(myArr);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+
+//Initialize Google Map API
+function initMap() {
+    // Styles a map in night mode.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 41.878, lng: -87.629 },
+        zoom: 8,
+        styles: [{
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#ebe3cd"
+                }]
+            },
+            {
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#523735"
+                }]
+            },
+            {
+                "elementType": "labels.text.stroke",
+                "stylers": [{
+                    "color": "#f5f1e6"
+                }]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry.stroke",
+                "stylers": [{
+                    "color": "#c9b2a6"
+                }]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "geometry.stroke",
+                "stylers": [{
+                    "color": "#dcd2be"
+                }]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#ae9e90"
+                }]
+            },
+            {
+                "featureType": "landscape.natural",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#dfd2ae"
+                }]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#dfd2ae"
+                }]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#93817c"
+                }]
+            },
+            {
+                "featureType": "poi.business",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#a5b076"
+                }]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#447530"
+                }]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#f5f1e6"
+                }]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#fdfcf8"
+                }]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#f8c967"
+                }]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [{
+                    "color": "#e9bc62"
+                }]
+            },
+            {
+                "featureType": "road.highway.controlled_access",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#e98d58"
+                }]
+            },
+            {
+                "featureType": "road.highway.controlled_access",
+                "elementType": "geometry.stroke",
+                "stylers": [{
+                    "color": "#db8555"
+                }]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#806b63"
+                }]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#dfd2ae"
+                }]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#8f7d77"
+                }]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "labels.text.stroke",
+                "stylers": [{
+                    "color": "#ebe3cd"
+                }]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#dfd2ae"
+                }]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#b9d3c2"
+                }]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#92998d"
+                }]
+            }
+        ]
+    });
+
+    // get_breweries();
+    var geocoder = new google.maps.Geocoder();
+    geocodeAddress(geocoder, map);
+    // var xmlhttp = new XMLHttpRequest();
+    // var url = "https://maps.googleapis.com/maps/api/geocode/json?address=1800+N+Clybourn+Ave,+Chicago,+IL&key=AIzaSyC9JsezK4dNu_-N1jQL0IGTdPTWd4bzG2o";
+
+    // xmlhttp.onreadystatechange = function() {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //         var coord = JSON.parse(this.responseText);
+
+    //         var marker = new google.maps.Marker({
+    //             position: coord.results[0].geometry.location,
+    //             map: map,
+    //             title: 'Goose Island'
+
+    //         });
+    //     }
+    // };
+    // xmlhttp.open("GET", url, true);
+    // xmlhttp.send();
+}
+
+
+function geocodeAddress(geocoder, resultsMap) {
+    var address = "1800 N Clybourn Ave, Chicago, IL";
+    geocoder.geocode({ 'address': address }, function(results, status) {
+        if (status === 'OK') {
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
